544e6949fbdf41327916d846d6c1f02d969cacb0
Philippe Martinet <philippe.martinet@it-gymnasiet.se>
2016-11-30 15:26:08 +0100

    Redone

--- tree.rb
+++ tree.rb
@@ -8,7 +8,7 @@ class TreeNode
     # Values greater than it will be inserted on its right
     def initialize val,left,right
         @value = val
-        @left = left34
+        @left = left
         @right = right
     end
 end
